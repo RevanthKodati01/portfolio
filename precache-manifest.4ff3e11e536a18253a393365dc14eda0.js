bb29eaf63ba6059a20066eacc95f3071958d121b
Kodati Revanth <kodatirevanth@Kodatis-Laptop.lan>
2024-10-12 03:00:54 -0400

    Updates

---
+++ precache-manifest.4ff3e11e536a18253a393365dc14eda0.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32a8ade3e5754481f746088d25186b3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "850a880f3ebbf38af10e",
+    "url": "/portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "573335a82b2797ac05f0",
+    "url": "/portfolio/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "c36d27bf983489c31134",
+    "url": "/portfolio/static/css/main.effb9528.chunk.css"
+  },
+  {
+    "revision": "ff26328a1607b07d5a1e",
+    "url": "/portfolio/static/js/2.5f8ae879.chunk.js"
+  },
+  {
+    "revision": "d4d701abc30fcef5f714584a64df6e82",
+    "url": "/portfolio/static/js/2.5f8ae879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "850a880f3ebbf38af10e",
+    "url": "/portfolio/static/js/3.45c6025e.chunk.js"
+  },
+  {
+    "revision": "573335a82b2797ac05f0",
+    "url": "/portfolio/static/js/4.263da7c1.chunk.js"
+  },
+  {
+    "revision": "c36d27bf983489c31134",
+    "url": "/portfolio/static/js/main.01ae1f4c.chunk.js"
+  },
+  {
+    "revision": "5421756ec1f0f58137c4",
+    "url": "/portfolio/static/js/runtime-main.2322f740.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "5fd71426f16ef3dcc76ebdb4c1ee607c",
+    "url": "/portfolio/static/media/resume.5fd71426.pdf"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
